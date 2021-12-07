a87bc42209f678c1d8be026a1901ee182c4bc1f4
Opilane TTHK <opilane@tthk.ee>
2021-12-07 15:59:39 +0200

    first commit

---
+++ BitCoinCalculator/BitCoinRate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitCoinCalculator
+{
+    public class BitCoinRate
+    {
+        public bpi bpi { get; set; }
+
+    }
+
+    public class bpi
+    {
+        public EUR EUR { get; set;  }
+
+    }
+
+    public class EUR
+    {
+        public string code { get; set;  }
+
+        public float rate_float { get; set; }
+
+    }
+
+}
+
+    
---
+++ BitCoinCalculator/Program.cs
@@ -0,0 +1,47 @@
+﻿ using System;
+using System.Net;
+
+
+namespace BitCoinCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitCoinRate currentBitCoin = GetRates();
+            Console.WriteLine($"Current rate : {currentBitCoin.bpi.EUR.code} {currentBitCoin.bpi.EUR.rate_float}");
+
+
+            Console.Write("How many bitcoins do you have? : ");
+            int bitcoin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("EUR/USD/GPB);
+        }
+
+        public static BitCoinRate GetRates()
+        {
+            string uri = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            HttpPWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.getResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            BitCoinRate bitcoinData;
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                bitcoinData = JsonConvert.DeserializeObject<BitCoinRate>(response);
+
+
+            }
+
+            return bitcoinData;
+
+
+
+
+        }
+
+    }
+}
